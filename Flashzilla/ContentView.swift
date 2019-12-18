990ea3daebec68bf076bb60887aaa528a364f1ec
Nick Polychronakis <nickpolychronakis@me.com>
2019-12-18 18:17:59 +0200

    Initial Commit

---
+++ Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by NICK POLYCHRONAKIS on 18/12/19.
+//  Copyright © 2019 NICK POLYCHRONAKIS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
